0b6b650febde2ad865b3b2d9b8bbd328f5182fb0
Kevin Thalmann <kevin_thalmann@yahoo.de>
2022-09-01 08:46:37 +0200

    V2

--- client.py
+++ client.py
@@ -1,37 +1,54 @@
-"""Kevin Thalmann
-"""
 import socket
 import threading
 
-nickname = input("Choose your Nickname: ")
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("10.3.164.44", 9090))
 
+class Client:
+    def __init__(self, auto_ip: bool = False, port: int = 9090) -> None:
+        """Init
 
-def receive():
-    """_summary_"""
-    while True:
-        try:
-            message = client.recv(1024).decode("utf-8")
-            if message == "NICK":
-                client.send(nickname.encode("utf-8"))
-            else:
-                print(message)
-        except Exception as ex:
-            print(f"an error occurred: {ex}")
-            client.close()
-            break
+        Args:
+            auto_ip (bool, optional): read the ip Automatic. Defaults to False.
+            port (int, optional): ip Port. Defaults to 9090.
+        """
+        self.nickname = input("Choose your Nickname: ")
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if auto_ip:
+            self.hostname = socket.gethostname()
+        else:
+            self.hostname = "10.3.164.44"
 
+        self.client.connect((self.hostname, port))
 
-def write():
-    """_summary_"""
-    while True:
-        message = f"{nickname}: {input('')}".encode("utf-8")
-        client.send(message)
+    def receive(self) -> None:
+        """Receive messages"""
+        while True:
+            try:
+                message = self.client.recv(1024).decode("utf-8")
+                # if the messae is "NEW" the nickname will be sendet to the server
+                if message == "NEW":
+                    self.client.send(self.nickname.encode("utf-8"))
+                # else print the message in cmd
+                else:
+                    print(message)
+            except Exception as ex:
+                print(f"an error occurred: {ex}")
+                self.client.close()
+                break
 
+    def write(self) -> None:
+        """Send messages"""
+        while True:
+            message = f"{self.nickname}: {input('')}".encode("utf-8")
+            self.client.send(message)
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+    def new_client(self) -> None:
+        """Create a thread for a client"""
+        receive_thread = threading.Thread(target=self.receive)
+        receive_thread.start()
+        write_thread = threading.Thread(target=self.write)
+        write_thread.start()
+        
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
+if __name__ == "__main__":
+    client = Client(auto_ip=True)
+    client.new_client()
--- server.py
+++ server.py
@@ -1,64 +1,82 @@
 import socket
 import threading
 
-Host = "10.3.164.44"
-Port = 9090
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((Host, Port))
-server.listen()
+class CreateServer:
+    def __init__(self, auto_ip: bool = False, port: int = 9090) -> None:
+        """Init
 
-clients = []
-nicknames = []
+        Args:
+            auto_ip (bool, optional): read the ip Automatic. Defaults to False.
+            port (int, optional): ip Port. Defaults to 9090.
+        """
+        if auto_ip:
+            self.hostname = socket.gethostname()
+        else:
+            self.hostname = "10.3.164.44"
+        self.port = port
+        # create a internet port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.hostname, self.port))
+        # server is listing
+        self.server.listen()
+        # list of clients and nicknames
+        self.clients = []
+        self.nicknames = []
+
+    def broadcast(self, mgs: str) -> None:
+        """Sends infomations at of clients
+
+        Args:
+            mgs (str): message
+        """
+        for client in self.clients:
+            # Send data to the socket. The socket must be connected to a remote socket.
+            client.send(mgs)
+
+    def handle(self, client: object) -> None:
+        """Receive a message from a client and send it so all other clients
+
+        Args:
+            client (object): client
+        """
+        while True:
+            try:
+                # Receive data from the socket. The return value is a bytes object
+                # representing the data received. The maximum amount of data to be
+                # received at once is specified by bufsize.
+                mgs = client.recv(1024)
+                self.broadcast(mgs)
+            except Exception:
+                # when a client leaves the server, a message is sent to all other clients
+                index = self.clients.index(client)
+                self.clients.remove(client)
+                nickname = self.nicknames[index]
+                self.nicknames.remove(nickname)
+                self.broadcast(f"{nickname} left the chat!".encode("utf-8"))
+                break
+
+    def receive(self) -> None:
+        """Create a tread for every client"""
+        print("server is listening ...")
+        while True:
+            client, address = self.server.accept()
+            print(f"connected with: {str(address)}")
+            # if an new clients join the server, "NEW" will be sendet to the Client
+            client.send("NEW".encode("utf-8"))
+            # the answer is the nickname of the client
+            nickname = client.recv(1024).decode("utf-8")
+            self.nicknames.append(nickname)
+            self.clients.append(client)
+            print(f"Nickname of the client is {nickname}")
+            # send to all clients
+            self.broadcast(f"{nickname} joined the chat!".encode("utf-8"))
+            # send only at client
+            client.send("Your are connected!\nSend your Message here...".encode("utf-8"))
+            thread = threading.Thread(target=self.handle, args=(client,))
+            thread.start()
 
 
-def broadcast(mgs):
-    """_summary_
-
-    Args:
-        mgs (_type_): _description_
-    """
-    for client in clients:
-        client.send(mgs)
-
-
-def handle(client):
-    """_summary_
-
-    Args:
-        client (_type_): _description_
-    """
-    while True:
-        try:
-            mgs = client.recv(1024)
-            broadcast(mgs)
-        except Exception:
-            index = clients.index(client)
-            clients.remove(client)
-            nickname = nicknames[index]
-            nicknames.remove(nickname)
-            broadcast(f"{nickname} left the chat!".encode("utf-8"))
-            break
-
-
-def receive():
-    """_summary_"""
-    while True:
-        client, address = server.accept()
-        print(f"connected with: {str(address)}")
-
-        client.send("NICK".encode("utf-8"))
-        nickname = client.recv(1024).decode("utf-8")
-        nicknames.append(nickname)
-        clients.append(client)
-
-        print(f"Nickname of the client is {nickname}")
-        broadcast(f"{nickname} joined the chat!".encode("utf-8"))
-        client.send("Your are connected!".encode("utf-8"))
-
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
-
-
-print("server is listening ...")
-receive()
+if __name__ == "__main__":
+    server = CreateServer(auto_ip=True)
+    server.receive()
